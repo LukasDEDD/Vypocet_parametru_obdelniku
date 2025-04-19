4db8a78521b01ec516807a8deeb91619c516e85d
LukasDD82 <lukas.simek.de@gmail.com>
2025-04-19 21:13:23 +0200

    uprava

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int stranaA = 10;
+        int stranaB = 5;
+
+        int obvod, obsah;
+            obvod = 2 * stranaA + 2 * stranaB;
+            obsah = stranaA * stranaB;
+
+            System.out.println( "Obvod obdelniku je " + obvod + " Obsah obdelniku je " + obsah );
         }
     }
-}
